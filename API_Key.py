f0bc3073243f288d4c6cf7cb6d2b41f39d6ed341
Red and Green <62441426+RGGH@users.noreply.github.com>
2020-05-18 19:50:07 +0100

    Example api key file
    Shows you the format of file

--- API_Key.py
+++ API_Key.py
@@ -1,5 +1,5 @@
 ''' Use with commentslist.py - replace with production key eventually'''
 
-# This is a made up API key just to show you the format!!
+# This is a made up API key just to show you the format - don't try and use, it won't work- replace with your key !
 
 myapi = "XXXaSyAPDShSNXXXnD6qvIrHXXX7cPXXX3NhXXX"
