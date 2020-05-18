e3931d219b9bd1bf7d5c13060eb346d9ff42fc9a
Red and Green <62441426+RGGH@users.noreply.github.com>
2020-05-18 19:46:11 +0100

    Create API_Key.py

---
+++ API_Key.py
@@ -0,0 +1,5 @@
+''' Use with commentslist.py - replace with production key eventually'''
+
+# This is a made up API key just to show you the format!!
+
+myapi = "XXXaSyAPDShSNXXXnD6qvIrHXXX7cPXXX3NhXXX"
